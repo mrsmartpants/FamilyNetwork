74baedc426d682c7b0ed6f6a6981cd6e16b6be7c
Ethan Veres <ethanveres@gmail.com>
2015-05-03 20:14:22 -0400

    initial commit

---
+++ app/config/environment/development.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  mysql: {
+    options: {
+      host: 'localhost:8889',
+      user: 'root',
+      password: 'root'
+    }
+  },
+
+  seedDB: true
+};
