fcb5b9b3bfcdf0df47b0448a35597c0278adf11b
Ethan Veres <ethanveres@gmail.com>
2015-05-05 16:56:09 -0400

    added some tests for user model

--- test/models/User.js
+++ test/models/User.js
@@ -1,3 +1,74 @@
-/**
- * Created by eytanveres on 5/5/15.
- */
+'use strict';
+
+var should = require('should');
+var app = require('../../app');
+var User = require('../../app/models').User;
+
+
+var userObject = {
+  provider: 'local',
+  firstName: 'Fake',
+  lastName: 'User',
+  email: 'fakeuser@email.com',
+  password: 'password'
+};
+
+var testUser = User.build(userObject);
+
+
+describe('User Model', function () {
+  before(function (done) {
+    // Clear users before testing
+    User.sync({force: true})
+      .then(function () {
+        done();
+      });
+  });
+
+  afterEach(function (done) {
+    User.sync({force: true})
+      .then(function () {
+        done();
+      });
+  });
+
+  it('should begin with no users', function (done) {
+    User.findAll()
+      .then(function (users) {
+        users.should.have.length(0);
+        done();
+      })
+      .catch(function (err) {
+        done()
+      });
+  });
+
+  it('should fail when saving a duplicate user', function (done) {
+    testUser.save()
+      .then(function () {
+        //create duplicate user
+        User.create(userObject)
+          .catch(function (err) {
+            should.exist(err);
+            done();
+          });
+      });
+  });
+
+  it('should fail when saving without an email', function (done) {
+    testUser.email = '';
+    testUser.save()
+      .catch(function (err) {
+        should.exist(err);
+        done();
+      });
+  });
+
+  it("should authenticate user if password is valid", function () {
+    return testUser.authenticate('password').should.be.true;
+  });
+
+  it("should not authenticate user if password is invalid", function () {
+    return testUser.authenticate('blah').should.not.be.true;
+  });
+});
