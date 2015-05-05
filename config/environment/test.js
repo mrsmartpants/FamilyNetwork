49fa2ee2ec72070329de75aeb2b780081b98e43c
Ethan Veres <ethanveres@gmail.com>
2015-05-05 16:55:43 -0400

    created test config file

--- config/environment/test.js
+++ config/environment/test.js
@@ -3,8 +3,22 @@
 // Test specific configuration
 // ===========================
 module.exports = {
-  //// MongoDB connection options
-  //mongo: {
-  //  uri: 'mongodb://localhost/familynetwork-test'
-  //}
+  sequelize: {
+    name: 'familynetwork-test',
+    user: 'root',
+    password: 'root',
+
+    options: {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: '8889',
+      logging: false,
+      pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+      }
+    }
+  }
+
 };
---
+++ test/models/User.js
@@ -0,0 +1,3 @@
+/**
+ * Created by eytanveres on 5/5/15.
+ */
