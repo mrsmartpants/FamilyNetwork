3732bd4ffe32b56fa27357ecb40de50bbad9cc70
Ethan Veres <ethanveres@gmail.com>
2015-05-05 09:57:51 -0400

    removed log and added return

--- config/passport/local.js
+++ config/passport/local.js
@@ -50,12 +50,11 @@ exports.load = function(router) {
   router.post('/', function(req, res, next) {
     passport.authenticate('local', function (err, user, info) {
       var error = err || info;
-      console.log(error);
       if (error) return  res.status(401).json(error);
       if (!user) return res.status(404).json({message: 'Something went wrong, please try again.'});
 
       var token = auth.signToken(user.id, user.role);
-      res.status(200).json({token: token});
+      return res.status(200).json({token: token});
     })(req, res, next)
   });
 
