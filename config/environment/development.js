1090492c16eac124b76cc04bacb04d54c2b52425
Ethan Veres <ethanveres@gmail.com>
2015-05-06 14:53:36 -0400

    using sequelize cli and its structure for migrations

--- config/environment/development.js
+++ config/environment/development.js
@@ -3,23 +3,5 @@
 // Development specific configuration
 // ==================================
 module.exports = {
-  sequelize: {
-    name: 'familynetwork',
-    user: 'root',
-    password: 'root',
-
-    options: {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: '8889',
-      logging: false,
-      pool: {
-        max: 5,
-        min: 0,
-        idle: 10000
-      }
-    }
-  },
-
   seedDB: true
 };
--- config/environment/test.js
+++ config/environment/test.js
@@ -3,22 +3,6 @@
 // Test specific configuration
 // ===========================
 module.exports = {
-  sequelize: {
-    name: 'familynetwork-test',
-    user: 'root',
-    password: 'root',
 
-    options: {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: '8889',
-      logging: false,
-      pool: {
-        max: 5,
-        min: 0,
-        idle: 10000
-      }
-    }
-  }
 
 };
